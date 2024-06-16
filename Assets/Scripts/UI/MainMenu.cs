4a535f78f4e8761b0fbf77d854d718daa67c4a16
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-06-16 08:26:26 +0630

    preparing for phase5.1

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+    public BgMusic bgm;
     public void QuitGame()
     {
+        bgm.Fade();
+        StartCoroutine(QuitTheGame());
+    }
+
+    IEnumerator QuitTheGame()
+    {
+        transition.SetTrigger("Start");
+
+        yield return Helpers.GetWait(1f);
+
         Application.Quit();
     }
 
