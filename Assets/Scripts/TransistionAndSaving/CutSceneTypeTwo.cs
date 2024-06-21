ada6ee11527347fe24d63535377fabb310b8cec9
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-06-21 13:53:04 +0630

    falling scene

---
+++ Assets/Scripts/TransistionAndSaving/CutSceneTypeTwo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class CutSceneTypeTwo : MonoBehaviour
+{
+    private PlayableDirector timeline;
+    [SerializeField] private string sceneName;
+    [SerializeField] private float waitSecond = 1f;
+    public BgMusic bgm;
+
+
+    private void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+        timeline.stopped += OnTimelineStopped;
+    }
+
+    private void OnTimelineStopped(PlayableDirector director)
+    {
+        bgm.Fade();
+        StartCoroutine(LoadNextScene());
+    }
+    IEnumerator LoadNextScene()
+    {
+        yield return Helpers.GetWait(waitSecond);
+        SceneManager.LoadScene(sceneName);
+    }
+}
